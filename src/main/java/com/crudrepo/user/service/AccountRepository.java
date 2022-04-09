74fb8cfb76e264e5a34e52ed15d60e666a5e71c0
GabrielStanescu <stanescu.gabriel2001@yahoo.com>
2022-04-09 21:36:43 +0300

    Added ibanGenerator and general logic
    Signed-off-by: GabrielStanescu <stanescu.gabriel2001@yahoo.com>

--- src/main/java/com/crudrepo/user/service/AccountRepository.java
+++ src/main/java/com/crudrepo/user/service/AccountRepository.java
@@ -3,5 +3,8 @@ package com.crudrepo.user.service;
 import com.crudrepo.user.model.Account;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends CrudRepository<Account, Integer> {
+    Optional<Account> findByIBAN(String iban);
 }
--- src/main/java/com/crudrepo/user/service/AccountService.java
+++ src/main/java/com/crudrepo/user/service/AccountService.java
@@ -1,14 +1,40 @@
 package com.crudrepo.user.service;
 
+import com.crudrepo.user.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class AccountService {
     @Autowired
     AccountRepository accountRepository;
 
-
     public void createAccount(Integer id, String currency) {
+    }
+
+    public String ibanGenerator() {
+        StringBuilder iban = new StringBuilder("RO"); // can be edited manually
+        Random rand = new Random();
+        int i, n;
+
+        while (true) {
+            for (i = 0; i < 14; i++) {
+                n = rand.nextInt(10);
+                iban.append(n);
+            }
+            if (!ibanDuplicate(iban.toString()))
+                break;
+            iban = new StringBuilder("RO");
+        }
+
+        return iban.toString();
+    }
+
+    public boolean ibanDuplicate(String iban) {
+        Optional<Account> op = accountRepository.findByIBAN(iban);
+        return op.isPresent();
     }
 }
