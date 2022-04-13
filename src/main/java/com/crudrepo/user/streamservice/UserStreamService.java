aa3ddd3b9cf1fe792d678e7b5aee15658aeef0fc
Gabriel <stanescu.gabriel2001@yahoo.com>
2022-04-13 21:42:27 +0300

    minor touches. final homework
    Signed-off-by: Gabriel <stanescu.gabriel2001@yahoo.com>

--- src/main/java/com/crudrepo/user/streamservice/UserStreamService.java
+++ src/main/java/com/crudrepo/user/streamservice/UserStreamService.java
@@ -5,10 +5,8 @@ import com.crudrepo.user.service.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 @Service
