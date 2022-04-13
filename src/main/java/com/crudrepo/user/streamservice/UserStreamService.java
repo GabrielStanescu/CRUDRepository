7287d4e227782d231c7ccb571d27fa5a7a9da323
Gabriel <stanescu.gabriel2001@yahoo.com>
2022-04-13 20:47:10 +0300

    new user service added. implemented getUsersInitials using streams
    Signed-off-by: Gabriel <stanescu.gabriel2001@yahoo.com>

---
+++ src/main/java/com/crudrepo/user/streamservice/UserStreamService.java
@@ -0,0 +1,25 @@
+package com.crudrepo.user.streamservice;
+
+import com.crudrepo.user.model.User;
+import com.crudrepo.user.service.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+@Service
+public class UserStreamService {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<String> getUsersInitials() {
+        Iterable<User> allUsers = userRepository.findAll();
+        return StreamSupport.stream(allUsers.spliterator(), false)
+                .map(user -> user.getFirstName().charAt(0) + user.getLastName().charAt(0) + "")
+                .collect(Collectors.toList());
+    }
+}
